cc4dc40595c18de8f80a2f3c5c6d6b58ca5e2f67
Donovan Vernon <d.vernon5@gmail.com>
2025-01-28 14:07:35 -0800

    Save local changes before pulling
    Refactor code instead of a monolithic approach I changed it using a functional approach. Reducing duplicates, and making it easier to understand.

--- main.cpp
+++ main.cpp
@@ -2,87 +2,144 @@
 #include <cstdlib>
 #include <ctime>
 
-void guessingGame (int, int, int);
+struct Status {
+    std::string status_code;
+};
+
+struct Rules {
+    int winningNumber = 0;
+    int guessTotal = 0;
+    int numAttempts = 0;
+};
+
+struct UserInput {
+    int guessingNumber = 0;
+    char replayGuessingGame = NULL;
+};
+
+void guessingGameTest(UserInput& user, Rules& features);
+void requestToPlayAgain(UserInput& user, Rules& features);
+void updateUserOnGameStatus(UserInput& user, Rules& features);
+void incrementGuessAttempts(Rules& features);
+void clearKeyboard();
+UserInput userGuessSelection(UserInput& user);
+UserInput userReplaySelection(UserInput& user);
+Status userGuessStatus(UserInput& user, Rules& features);
+bool isGameWinner(UserInput& user, Rules& features);
+bool isGuessingNumberTooHigh(UserInput& user, Rules& winningNumber);
+bool areGuessesAvailable(Rules& features);
+bool userWantsReplay(UserInput& user);
 
 int main(void)
 {
     std::srand((unsigned) std::time(0));
-    int winningNumber = (std::rand() % 100) + 1;
-    const int guessingAttemptsAmount = 5;
-    int numberOfAttempts = 0;
+    Rules features;
+    features.winningNumber = (std::rand() % 100) + 1;
+    features.guessTotal = 5;
+    features.numAttempts = 1;
+    UserInput user;
+    guessingGameTest(user, features);
     
-    guessingGame(winningNumber, guessingAttemptsAmount, numberOfAttempts);
-    
-    return 0;
+    return EXIT_SUCCESS;
 }
 
-void guessingGame (int winningNumber, int guessingAttemptsAmount, int numberOfAttempts)
-{
-    int guessingNumber = 0;
-    int replayGuessingGame = 0;
-    
-    while (1)
-    {
-        std::cout << "Enter a number between 1 and 100. If you guess the right number you win a prize: ";
-        std::cin  >> guessingNumber;
-        
-        if (guessingNumber < 1 || guessingNumber > 100)
-        {
-            std::cout << "You enter an invalid number.\n";
-            continue;
-        } else if (guessingNumber == winningNumber) {
-            std::cout << "Congratulations!! You won on " << (numberOfAttempts + 1) << " attempts!" << std::endl;
-            std::cout << "Would you like to play again? if yes enter 1, no enter 0: ";
-            std::cin  >> replayGuessingGame;
-            
-            while (replayGuessingGame < 0 || replayGuessingGame > 1)
-            {
-                std::cout << "Invalid number. Please enter 1 to replay or 0 to exit: ";
-                std::cin  >> replayGuessingGame;
-            }
-            if (replayGuessingGame == 1)
-            {
-                numberOfAttempts = 0;
-                std::srand((unsigned) std::time(0));
-                winningNumber = (std::rand() % 100) + 1;
-                continue;
-            } else if (replayGuessingGame == 0) {
-                std::cout << "You are leaving the game. We hope to see you again." << std::endl;
-                break;
-            }
-        } else if (guessingNumber > winningNumber) {
-            std::cout << guessingNumber << " is too high." << std::endl;
-            std::cout << "You have " << (guessingAttemptsAmount - numberOfAttempts - 1) << " attempts left." << std::endl;
-        } else if (guessingNumber < winningNumber) {
-            std::cout << guessingNumber << " is too low." << std::endl;
-            std::cout << "You have " << (guessingAttemptsAmount - numberOfAttempts - 1) << " sttempts left." << std::endl;
-        }
-        
-        ++numberOfAttempts;
-        
-        if (numberOfAttempts == guessingAttemptsAmount)
-        {
-            std::cout << "Gameover! Winning number is " << winningNumber << std::endl;
-            std::cout << "Would you like to play again? If yes enter 1, no enter 0: ";
-            std::cin  >> replayGuessingGame;
-            
-            while (replayGuessingGame < 0 || replayGuessingGame > 1)
-            {
-                std::cout << "Invalid number. Please enter 1 to replay or 0 to exit: ";
-                std::cin  >> replayGuessingGame;
-            }
-            
-            if (replayGuessingGame == 1)
-            {
-                numberOfAttempts = 0;
-                std::srand((unsigned) std::time(0));
-                winningNumber = (std::rand() % 100) + 1;
-                continue;
-            } else if (replayGuessingGame == 0)
-            {
-                std::cout << "You are now leaving the game. We hope to see you again!" << std::endl;
-                break;
-            }
+void guessingGameTest(UserInput& user, Rules& features) {
+    while (true) {
+        user = userGuessSelection(user);
+        Status status = userGuessStatus(user, features);
+        if (status.status_code == "Winner") {
+            std::cout << "Congratulations!! You won on " << (features.numAttempts) << " attempts!" << std::endl;
+            requestToPlayAgain(user, features);
+        } else if (status.status_code == "Incorrect") {
+            updateUserOnGameStatus(user, features);
+            incrementGuessAttempts(features);
+        } else {
+            std::cout << "Gameover! Winning number is " << features.winningNumber << std::endl;
+            requestToPlayAgain(user, features);
         }
     }
+}
+
+UserInput userGuessSelection(UserInput& user) {
+    std::cout << "Enter a number between 1 and 100. If you guess the right number you win a prize: ";
+    while (!(std::cin >> user.guessingNumber) || user.guessingNumber < 0 || user.guessingNumber > 100) {
+        std::cerr << "Error! Please enter a number from 1 to 100: ";
+        clearKeyboard();
+    }
+    clearKeyboard();
+    
+    return user;
+}
+
+void updateUserOnGameStatus(UserInput& user, Rules& features) {
+    if (isGuessingNumberTooHigh(user, features)) {
+        std::cout << user.guessingNumber << " is too high." << std::endl;
+    } else {
+        std::cout << user.guessingNumber << " is too low." << std::endl;
+    }
+    
+    std::cout << "You have " << (features.guessTotal - features.numAttempts) << " attempts left." << std::endl;
+}
+
+void incrementGuessAttempts(Rules& features) {
+    ++features.numAttempts;
+}
+
+void requestToPlayAgain(UserInput& user, Rules& features) {
+    user = userReplaySelection(user);
+    if (userWantsReplay(user)) {
+        features.numAttempts = 1;
+        std::srand((unsigned) std::time(0));
+        features.winningNumber = (std::rand() % 100) + 1; // 100;
+    } else {
+        std::cout << "Thank you for playing! Please come again." << std::endl;
+        exit(EXIT_SUCCESS);
+    }
+}
+
+UserInput userReplaySelection(UserInput& user) {
+    std::cout << "Would you like to play again? if yes enter (y or Y). To exit enter (n or N): ";
+    while (!(std::cin >> user.replayGuessingGame) ||
+           (user.replayGuessingGame != 'y' && user.replayGuessingGame != 'Y' &&
+            user.replayGuessingGame != 'n' && user.replayGuessingGame != 'N')) {
+        std::cerr << "Invalid selection. To replay please enter (y or Y) or (n or N) to exit: ";
+        clearKeyboard();
+    }
+    clearKeyboard();
+    
+    return user;
+}
+
+void clearKeyboard() {
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.clear();
+}
+
+Status userGuessStatus(UserInput& user, Rules& features) {
+    Status status;
+    if (isGameWinner(user, features)) {
+        status.status_code = "Winner";
+    } else if (areGuessesAvailable(features)) {
+        status.status_code = "Incorrect";
+    } else {
+        status.status_code = "Gameover";
+    }
+    
+    return status;
+}
+ 
+bool isGameWinner(UserInput& user, Rules& features) {
+    return user.guessingNumber == features.winningNumber;
+}
+
+bool areGuessesAvailable(Rules& features) {
+    return features.numAttempts != features.guessTotal;
+}
+
+bool userWantsReplay(UserInput& user) {
+    return (user.replayGuessingGame == 'y' || user.replayGuessingGame == 'Y');
+}
+
+bool isGuessingNumberTooHigh(UserInput& user, Rules& features) {
+    return user.guessingNumber > features.winningNumber;
 }
